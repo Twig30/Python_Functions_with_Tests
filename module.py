47d75ccd728081d31b861e2697b8c83fe0f3524f
Twig30 <immanuel.delgado@stu.vertexacademies.org>
2026-03-03 19:19:23 +0000

    added additional code to module

--- module.py
+++ module.py
@@ -1,14 +1,34 @@
 # two_sum takes two integer arguments and returns the sum of the integers
-def two_sum(a: int, b: int) -> int:    
-    pass
+def two_sum(a: int, b: int) -> int:
+    tsum = a + b
+    return tsum
+
+    
 
 # remove_vowels takes a string argument and return a string with all vowels removed
 def remove_vowels(s: str) -> str:
-    pass
+    letters = ["A", "E" "I", "O", "U"]
+    for char in s:
+        if s.upper == char:
+            s.replace(char ," ")
+            
+    return s
+
+    
+
+
+    
+
+
+    
+
+
+    
 
 # factorial takes an integer argument and returns the factorial result of that integer
 # factorial must use iteration and not the built-in factorial function
 def factorial(num: int) -> int:
+    
     pass
 
 # listify takes a string argument and returns a list of characters in the string
